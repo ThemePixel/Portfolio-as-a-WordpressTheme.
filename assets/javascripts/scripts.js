6721ef6325746f46131b58908ec719db63e2e94a
z9110858@gmail.com <z9110858@gmail.com>
2022-10-01 16:37:34 +0700

    menu pure js done

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -56,3 +56,33 @@
             menuBody.classList.toggle('_active');
         });
     }
+
+
+
+    const menuLinks = document.querySelectorAll('.menulink_li-a[data-goto], .menulink_sub_li-a[data-goto]');
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener("click", onMenuLinkClick);
+        });
+    
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+    
+                if (iconMenux.classList.contains('_active')) {
+                    document.body.classList.remove('_lock');
+                    iconMenux.classList.remove('_active');
+                    navMenu.classList.remove('_active');
+    
+                }
+    
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            }
+        }
+    }
