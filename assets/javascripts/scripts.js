4dba4e0c729ed526816f763af755aa21b371105e
z9110858@gmail.com <z9110858@gmail.com>
2022-10-01 15:41:46 +0700

    @media only screen and (min-width: 1px) {

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -43,3 +43,16 @@
         
         document.body.classList.add('_pc');
     }
+
+
+
+    const iconMenu = document.querySelector('.menu__icon');
+    const menuBody = document.querySelector('.menu__body');
+    if (iconMenu) {
+        
+        iconMenu.addEventListener("click", function (e) {
+            document.body.classList.toggle('_lock');
+            iconMenu.classList.toggle('_active');
+            menuBody.classList.toggle('_active');
+        });
+    }
